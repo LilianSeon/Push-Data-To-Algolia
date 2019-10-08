5328244193ec1e32c85bd3ccfcbbc5fca91e87e7
SEON Lilian TR/QEV <NNBJ0574@orange.com>
2019-10-08 13:54:04 +0200

    Push Data To Algolia php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// By using composer require algolia/algoliasearch-client-php.
+require 'vendor/autoload.php';
+
+// Create an indence of the client with Application ID + Admin API Key. (Information avalable on your Algolia Dashboard).
+$client = Algolia\AlgoliaSearch\SearchClient::create('ApplicationID', 'AdminAPIKey');
+
+// Initialized the index where you want to upload your data to.
+// If the index doesn't exist yet, it will be automatically created.
+$index = $client->initIndex('Index_Name');
+
+// Fetch your JSON file. 
+$jsonFile = json_decode(file_get_contents('example.json'), true);
+
+// Once you have your records ready, you can then push them to Algolia using the addObjects method.
+$index->saveObjects($jsonFile, ['autoGenerateObjectIDIfNotExist' => true]);
+
+?>
